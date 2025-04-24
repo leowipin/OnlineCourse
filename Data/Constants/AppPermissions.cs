0e7ef189d08df266601721c2199ea3680e7a07fc
leowipin <leonardopincayjaramillo@gmail.com>
2025-04-23 23:58:24 -0500

    roles and permissions 1

--- Data/Constants/AppPermissions.cs
+++ Data/Constants/AppPermissions.cs
@@ -4,7 +4,34 @@ public static class AppPermissions
 {
     public static class Courses
     {
+        public const string Create = "Permissions.Courses.Create";
+        public const string Read = "Permissions.Courses.Read";
+        public const string Update = "Permissions.Courses.Update";
+        public const string Delete = "Permissions.Courses.Delete";
 
+        public const string ReadOwn = "Permissions.Courses.ReadOwn";
+        public const string UpdateOwn = "Permissions.Courses.UpdateOwn";
+        public const string DeleteOwn = "Permissions.Courses.DeleteOwn";
+
+        public const string ManageAll = "Permissions.Courses.ManageAll";
     }
+    public static class Enrollments
+    {
+        public const string Create = "Permissions.Enrollments.Create";
+        public const string Read = "Permissions.Enrollments.Read";
+        public const string Update = "Permissions.Enrollments.Update";
+        public const string Delete = "Permissions.Enrollments.Delete";
 
+        public const string ReadOwn = "Permissions.Enrollments.ReadOwn";
+        public const string UpdateOwn = "Permissions.Enrollments.UpdateOwn";
+        public const string DeleteOwn = "Permissions.Enrollments.DeleteOwn";
+
+        public const string ManageAll = "Permissions.Enrollments.ManageAll";
+    }
+    public static class Users
+    {
+        public const string Read = "Permissions.Users.Read";
+
+        public const string Manage = "Permissions.Users.Manage";
+    }
 }
--- Data/Constants/AppRoles.cs
+++ Data/Constants/AppRoles.cs
@@ -5,4 +5,8 @@ public static class AppRoles
     public const string Instructor = "Instructor";
     public const string Student = "Student";
     public const string Admin = "Admin";
+
+    public static readonly Guid InstructorRoleId = Guid.Parse("2b04f320-d015-458d-aea4-36fc2e21a3f3");
+    public static readonly Guid StudentRoleId = Guid.Parse("0ed6fc08-9ae5-4302-bf34-b4e40b832f71");
+    public static readonly Guid AdminRoleId = Guid.Parse("35d30a2a-d3b7-40d4-9c5e-cd2d4eda950e");
 }
